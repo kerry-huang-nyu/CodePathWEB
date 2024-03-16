8b9aee9757003f1174da8df14cc9c1b371b784ba
kerryhuang <yh4558@nyu.edu>
2024-03-16 13:13:41 +0400

    add finished project

--- HW 3/flashcard/src/App.jsx
+++ HW 3/flashcard/src/App.jsx
@@ -7,52 +7,64 @@ import random from 'random';
 
 const App = () => {
   //read your files!!
+  let TOTAL = 300;
 
-  var pool = Array.from({ length: 300 }, (_, index) => index);
+  const [pool, setPool] = useState(Array.from({ length: 300 }, (_, index) => index));
 
-  /*
-  Error with left and curr! they are reset to these initial values each time? 
-  Do i need to update them through hooks? possibly... 
+  const [curr, setCurr] = useState(0);
 
-  */
-  var left = 300;
-  var curr = 0;
+  const [left, setLeft] = useState(TOTAL);
 
-  var history = [];
+  const [history, setHistory] = useState([]); //history should be a list of integers 
+
 
   const chooseNum = () => {
     let ind = random.int(0, left-1);
     let realind = pool[ind]; //the real index is stored in the pool 
-    pool[ind] = pool[left-1]; //puts the last value in the middle and then decrements 
-    left --;
-    history.push(realind);
+    var poolcopy = [...pool]; //copy pool 
+    poolcopy[ind] = pool[left-1]; //swap the indices 
+    setPool(poolcopy);
+
+    setLeft(prevLeft => prevLeft - 1);
+
+    var historycopy = [...history];
+    historycopy.push(realind);
+    setHistory(historycopy);
+    console.log("i have set history to be ", history);
+    console.log("i have set history to be ", history);
     return realind;
   }
 
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
   //setup a random number generator 
   //choose a random question 
   const [fact, setState] = useState(["Please clickme :D to reveal the answer", "42", 1]);
 
-  const [correct, setCorrect] = useState(true);
+  const [correct, setCorrect] = useState("unknown");
 
   const next = () =>{
+    reset();
     if (left > 0) {
       let ind = chooseNum();
       var tempState = [myObject[ind]['Question'], myObject[ind]['Answer'], myObject[ind]['Correct']];
-      setCount(count + 1);
+      setCount(prevCount => prevCount + 1) // Return the updated state value);
       setState(tempState);
-      curr += 1; 
-      console.log("now curr is " +  curr)
-      console.log("now left is ", left)
+      setCurr(count + 1); //why can't i just use setCurr(prevCount)
+
+      console.log("now count is " +  count);
+      console.log("now curr is " +  curr);
+      console.log("now left is ", left);
+      console.log("the history is ", history);
     }
   }
 
   const prev = () =>{
-    if (count > 0 && curr > 0){
-      curr --;
-      var ind = history[curr];
+    if (curr > 0){
+      setCurr(prevCurr => prevCurr - 1);
+      var newcurr = curr - 1;
+
+      var ind = history[newcurr];
       var tempState = [myObject[ind]['Question'], myObject[ind]['Answer'], myObject[ind]['Correct']];
       setState(tempState);
     }
@@ -61,15 +73,18 @@ const App = () => {
   const checkAnswer = () =>{
     var userAnswer = document.getElementById('answer').value;
     var correctAnswer = fact[1];
-    setCorrect(userAnswer === correctAnswer);
-
-
-    console.log(correct);
+    if (userAnswer === correctAnswer){
+      setCorrect("correct");
+    }
+    else{
+      setCorrect("wrong");
+    }
   }
 
   const reset = () => {
-
-
+    setCorrect("unknown");
+    var inputfield = document.getElementById('answer');
+    inputfield.value = '';
   }
 
   return (
@@ -80,13 +95,16 @@ const App = () => {
 
       <div className ="buttonContain">
 
-      <button onClick={prev}>← prev</button>
+      <button onClick={prev}>← Prev</button>
 
+      <div className='label'> 
       <label for="answer">Input: </label>
-      <input type="text" id='answer' ></input>
+      <input type="text" className={correct} id="answer" ></input>
+      </div>
+
       <button onClick={checkAnswer}>Submit</button>
 
-      <button onClick={next}>next →</button>
+      <button onClick={next}>Next →</button>
       </div>
       
 
